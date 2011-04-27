6c08945cdd83913ec807f467cfaea8f26011f042
Rainer Borene <rainerborene@gmail.com>
2011-04-27 15:57:22 -0300

    upgrade func and themes stuff

--- install.sh
+++ install.sh
@@ -8,6 +8,21 @@
 # My vim life.
 # Rainer Borene
 
+# Utility for updating themes
+if [ "$1" == "upgrade" ]; then
+  cd ~/.vim/colors
+  function theme() {
+    curl --url $1 -O > /dev/null 2>&1
+  }
+  echo "*** Updating themes..."
+  theme "https://github.com/nelstrom/vim-blackboard/raw/master/colors/blackboard.vim"
+  theme "https://github.com/nelstrom/vim-mac-classic-theme/raw/master/colors/mac_classic.vim"
+  theme "https://github.com/tpope/vim-vividchalk/raw/master/colors/vividchalk.vim"
+  theme "http://blog.toddwerth.com/entry_files/8/ir_black.vim"
+  echo "*** Done"
+  exit 1
+fi
+
 # Check if git is installed.
 if ! type -p git &> /dev/null; then
   echo "What? You don't have Git installed."
@@ -18,7 +33,7 @@ fi
 # See http://petdance.com/ack/ for more information.
 if ! type -p ack &> /dev/null; then
   echo "*** Installing ack..."
-  sudo curl http://betterthangrep.com/ack-standalone -o /usr/local/bin/ack
+  sudo curl http://betterthangrep.com/ack-standalone -o /usr/local/bin/ack > /dev/null 2>&1
   sudo chmod 0755 /usr/local/bin/ack
 fi
 
