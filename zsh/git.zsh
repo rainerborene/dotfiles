df124a84443fc764ccd313a353a1ff1ae7232d3f
Rainer Borene <rainerborene@gmail.com>
2012-07-11 17:52:42 -0300

    Add NrrwRgn bundle.

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,11 +1,22 @@
 # git
-alias gs='git status'
-alias gd='git diff --color'
-alias gdc='git diff --cached --color'
-alias gl='git log --oneline --color --max-count=15 --decorate'
-alias gu='git pull'
-alias gt='git tag'
-alias gsm='git submodule'
+alias gd="git diff | mate"
+alias ga="git add"
+alias gbd="git branch -D"
+alias gs="git status -s"
+alias gst="git status"
+alias gca="git commit -a -m"
+alias gm="git merge --no-ff"
+alias gpt="git push --tags"
+alias gp="git push"
+alias grh="git reset --hard"
+alias gb="git branch"
+alias gcob="git checkout -b"
+alias gco="git checkout"
+alias gba="git branch -a"
+alias gcp="git cherry-pick"
+alias gl="git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
+alias gpom="git pull origin master"
+alias gcd='cd "`git rev-parse --show-toplevel`"'
 alias gitv='vim .git/index -c "Gitv --all" -c "tabonly"'
 
 # commit pending changes and quote all args as message
